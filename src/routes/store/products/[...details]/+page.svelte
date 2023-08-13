8dfc62bdc294c0dfe858afa5f6c04029e86dc5ea
wyao <willjarvin@gmail.com>
2023-08-13 10:34:31 +0800

    feat: rest route params

---
+++ src/routes/store/products/[...details]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let data;
+</script>
+
+<h4>Product page</h4>
+{#if data.details}
+	<p class="red">{data.details}</p>
+{:else}
+	<p>No product selected! Try clicking one or adding your own URL</p>
+{/if}
+
+<style>
+	.red {
+		color: red;
+		font-weight: bold;
+	}
+</style>
